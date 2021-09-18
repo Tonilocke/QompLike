1935661f84fa884473947b343b1e4c2f928db280
Antonio <tonilocke@hotmail.it>
2021-09-18 03:10:40 +0200

    Added some changes to movement

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let screen = document.querySelector(".screen");
-let UP = 4;
-let FORWARD = 4;
+let UP = 2;
+let FORWARD = 2;
 
 
 class Qomp{
   constructor(){
     this.left = 0
-    this.bottom = 480
+    this.bottom = 432
     this.visual = document.createElement("div")
     const visual = this.visual
     visual.classList.add("qomp")
@@ -14,19 +14,29 @@ class Qomp{
     visual.style.bottom = this.bottom
     screen.appendChild(visual)
   }
+
   gravity(){
     setInterval(()=>{
 
         this.bottom -= UP;
-        this.visual.style.bottom = qomp.bottom +"px";
+        this.visual.style.bottom = this.bottom +"px";
+        this.left += FORWARD;
+        this.visual.style.left = this.left + "px";
         document.addEventListener("keydown",(e)=>{
-          if(e.keyCode == 32){
-            UP = -UP;
+        if(e.keyCode == 32){
+            UP = -UP;//spacebar -> 32
+            console.log("Spacebar");
+          }
+          if(e.key == "d"){//D -> 68
+            FORWARD = -FORWARD;
+            console.log("D");
           }
         })
-      if(this.bottom == 0)
+      if(this.bottom <= 0 || this.bottom > 448)
         UP = -UP;
-    },60);
+      if(this.left <= 0 || this.left >= 688)
+        FORWARD = -FORWARD;
+    },5);
   }
 }
 
